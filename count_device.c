d50b801592ffaee27cca5fbb99d143152431be83
Millad Ghane <mghane2@uhpc.hpcc.uh.edu>
2017-05-11 12:00:59 -0500

    adding enum

--- count_device.c
+++ count_device.c
@@ -16,8 +16,26 @@ cudaError_t cudaSetDevice ( int  device ) {
 	return orig_cudaSetDevice(device);
 }
 
+
+cudaError_t (*orig_cudaMemcpy)( void* dst, const void* src, size_t count, enum cudaMemcpyKind kind );
+
+__host__
+cudaError_t cudaMemcpy ( void* dst, const void* src, size_t count, enum cudaMemcpyKind kind ) {
+        FILE *f = fopen("deviceCounter.txt", "a");
+        fprintf(f, "Memcpy: %p -> %p   [%d]\n", src, dst, (int)count);
+        fclose(f);
+
+	return orig_cudaMemcpy(dst, src, count, kind);
+}
+
+
+
+
+
 void _init(void) {
 	printf("---------========== Loading overloaded CUDA functions...  ==============---------------\n");
 	orig_cudaSetDevice = (cudaError_t (*)(int)) dlsym(RTLD_NEXT, "cudaSetDevice");
+        orig_cudaMemcpy = (cudaError_t (*)(void*, const void*, size_t , enum cudaMemcpyKind)) dlsym(RTLD_NEXT, "cudaMemcpy");
+
 }
 
